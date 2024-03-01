39ed8c59eb5b104c4dbca9a551f43210f69f5460
Patrick Van Weelden <pvanweel@yahoo.com>
2024-03-01 16:16:29 -0600

    created current user feed endpoint

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -8,6 +8,7 @@ import { ArticleResponseInterface } from './types/articleResponse.interface';
 import slugify from 'slugify';
 import { ArticlesResponseInterface } from './types/articlesResponse.interface';
 import { UserService } from '@app/user/user.service';
+import { FollowEntity } from '@app/profile/profile.entity';
 
 @Injectable()
 export class ArticleService {
@@ -16,6 +17,8 @@ export class ArticleService {
     private readonly articleRepository: Repository<ArticleEntity>,
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly folllowRepository: Repository<FollowEntity>,
     private readonly dataSource: DataSource,
   ) {}
 
@@ -207,6 +210,43 @@ export class ArticleService {
     }
 
     return article;
+  }
+
+  async getFeed(
+    currentUserId: number,
+    query: any,
+  ): Promise<ArticlesResponseInterface> {
+    const follows = await this.folllowRepository.find({
+      where: {
+        followerId: currentUserId,
+      },
+    });
+
+    if (follows.length === 0) {
+      return { articles: [], articlesCount: 0 };
+    }
+
+    const followingUserIds = follows.map((follow) => follow.followingId);
+    const queryBuilder = this.dataSource
+      .getRepository(ArticleEntity)
+      .createQueryBuilder('articles')
+      .leftJoinAndSelect('articles.author', 'author')
+      .where('articles.authorId in (:...ids)', { ids: followingUserIds });
+
+    queryBuilder.orderBy('articles.createdat', 'DESC');
+
+    const articlesCount = await queryBuilder.getCount();
+
+    if (query.limit) {
+      queryBuilder.limit(query.limit);
+    }
+
+    if (query.offset) {
+      queryBuilder.offset(query.offset);
+    }
+
+    const articles = await queryBuilder.getMany();
+    return { articles, articlesCount };
   }
 
   buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
